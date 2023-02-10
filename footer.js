fabfe738f9dea4866d33127734db063bc017513e
Ashish Kumar <96014503+ashish-parjapat@users.noreply.github.com>
2023-02-10 19:47:42 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,29 @@
+
+
+function Footer(){
+    return(<section id="footer">
+    <div class="grid grid-cols-2">
+        <div>
+            <div class="container">
+                <p>Prompt Generator</p>
+                <p>Dweep Daily</p>
+                <p>All Contributors</p>
+                <p>Your data on Dweep.io</p>
+                <p>Contribute to Dweep</p>
+            </div>
+        </div>
+        <div>
+            <p> Dweep.io</p>
+            <p>Made with love in India</p>
+            <div class="icons">
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+            <p>hello@dweep.io</p>
+
+
+        </div>
+    </div>
+</section>)
+}
+export default Footer
