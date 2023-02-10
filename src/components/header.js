a9e66c7e535b61c0516499f0549324c2c467fd6d
Ashish Kumar <ashish.kalyan007@gmail.com>
2023-02-10 20:57:16 +0530

    final final

--- src/components/header.js
+++ src/components/header.js
@@ -3,26 +3,33 @@
 function Header(){
 
     return(
-    <section id="header">
-
-    <div class="row">
+        <section id="header">
+        <div class="grid grid-cols-2 gap-28">
+            
+            <div>
+                <div class="grid-cols-3">
+                    <div class="heading">  <h4>An inspiring design delivered to your inbox every morning</h4></div>
+             <div class="paragraph">Our team scouts the internet for the best designs, illustrations and art and delivers a truly inspiring one every day to your inbox</div>
+               <div class="subhead">    <h5>Show me how it looks</h5></div>
+              
         
-        <div class="col-lg-6">
-            <div class="container">
-            <h4>An inspiring design delivered to your inbox every morning</h4>
-            <p>Our team scouts the internet for the best designs, illustrations and art and delivers a truly inspiring one every day to your inbox</p>
-            <h5>Show me how it looks</h5>
-            <input class="input" type="email" name="email" placeholder="Your email address" />
-            <button type="button" class="btn btn-dark ">Dark</button>
-            <h6>Free - No Spam - No Data Sharing</h6>
+                <div> <input class="input" type="email" name="email" placeholder="Your email address"/>
+        
+                 
+                    <button class="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Button
+                      </button></div>
+                      <div class="free">  <h6>Free - No Spam - No Data Sharing</h6></div>
+               
+              
+            </div>
+            </div>
+            <div>
+                <img src="https://www.shutterstock.com/image-photo/jakarta-december-1-2022-tom-260nw-2233336333.jpg" />
+            </div>
+           
         </div>
-        </div>
-        <div class="col-lg-6">
-            <img src="https://www.shutterstock.com/image-illustration/cute-caroon-fox-cook-hat-260nw-1965347353.jpg"/>
-        </div>
-       
-    </div>
-        </section>
+            </section>
     )
 }
 export default Header
